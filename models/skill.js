e9ab78718349cf87cf9c4978aef5f5980e666232
Kristone Brown <Keyystone4@gmail.com>
2023-02-14 20:49:12 -0600

    skills

---
+++ models/skill.js
@@ -0,0 +1,20 @@
+const skills = [
+    {id: 125223, skill: 'HTML', done: true},
+   {id: 127904, skill: 'CSS', done: false},
+   {id: 139608, skill: 'JavaScript', done: false}
+];
+  
+  module.exports = {
+    getAll,
+    getOne
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
+  function getOne(id) {
+    id = parseInt(id)
+
+    return skills.find(skill => skill.id === id);
+  }
